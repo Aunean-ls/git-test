e550523de3067d9e20a071fa71ae8a8fd61932ab
Aunean-ls <1453357375@qq.com>
2022-02-16 10:58:30 +0800

    first commit

---
+++ src/main/java/top/shiyiri/git/GitTest.java
@@ -0,0 +1,11 @@
+package top.shiyiri.git;
+
+/**
+ * @author Aunean
+ * @date 2022/2/16 10:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
